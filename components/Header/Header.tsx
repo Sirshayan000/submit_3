a99456f22c66b98bbf627b07ce90530386ad75c6
Sirshayan000 <sirshan000@gmail.com>
2024-10-21 15:05:27 +0500

    applyin tailwind

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+             <div className="background-color: rgb(255 255 255); ">
+                <p>FREE AND EASAY RETURNS</p>
+                <a >SEE DETAILS</a>
+            </div>  
+             
+        </header>
+    )
+}
