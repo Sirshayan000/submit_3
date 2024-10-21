22d022988e8ba4aab3c574303848a2d218010ca4
Sirshayan000 <sirshan000@gmail.com>
2024-10-21 15:12:49 +0500

    impr

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,8 @@
 export default function Header(){
     return(
         <header>
-             <div className="background-color: rgb(255 255 255); ">
+             <div className="background-color: rgb(255 255 255); font-size: 1.25rem;
+             display: flex; justify-content: center; align-items: center">
                 <p>FREE AND EASAY RETURNS</p>
                 <a >SEE DETAILS</a>
             </div>  
