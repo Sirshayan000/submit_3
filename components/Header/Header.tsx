6ee3227e7885d089731ac54c8fbf4a9e3d66cd33
Sirshayan000 <sirshan000@gmail.com>
2024-10-21 16:53:06 +0500

    gooo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-           <div style={{
+           <div  className="offer" style={{
                 backgroundColor: 'rgb(255, 255, 255)',
                 fontSize: '0.9rem',
                 display: 'flex',
@@ -9,16 +9,40 @@ export default function Header(){
                 alignItems: 'center',
                 height: '2.5rem',
                 color: 'rgb(0, 0, 0)'
-            }}>
-                <p style={{
-                    fontWeight: 'bolder',
+                }}>
 
+                <p style={{
+                    fontWeight: '800%',
                 }}
                 >FREE AND EASAY RETURNS</p>
 
-                <a 
+                <a style={{
+                    fontWeight: '800%',
+                    color: 'rgb(0, 0, 0)',
+                    marginLeft: '1.25rem',              
+                }}
                 >SEE DETAILS</a>
-            </div>  
+          </div>  
+{/* --------------------------------------------- */}
+          <div className="navbar" style={{
+                    backgroundColor: 'rgb(0,0,0)',
+                    color:'rgb(255, 255, 255)',
+                    height: '5rem',
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>                        
+                 <div >
+                  <a href="#">Gift Items</a>
+                  <a href="#">Women</a>
+                  <a href="#">Men</a>
+                  <a href="#">Kids</a>
+                  <a href="#">Sale</a>         
+                </div>
+          </div>     
+                
+                
+                  
+            
              
         </header>
     )
