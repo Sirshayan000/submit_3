f904c80dbc1d2b87519dc02287006b26f47084b5
Sirshayan000 <sirshan000@gmail.com>
2024-10-22 17:27:57 +0500

    bac

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,46 @@
+export default function Footer(){
+    return(
+        <footer>
+            <div className="footer">
+                <ul className="list_1">
+                    <li><h3>Support</h3></li>
+                    <li>Contact Us</li>                    
+                    <li>Shipping and Delivery</li>
+                    <li>Return Policy</li>
+                    <li>Terms & Conditions</li>
+                    <li>Privacy Policy</li>
+                    <li>Service Discount</li>
+                </ul>
+
+                <ul className="list_2">                 
+                    <li>FAQ</li>
+                    <li>Shipping and Delivery</li>
+                    <li>Return Policy</li>
+                    <li>Terms & Conditions</li>
+                    <li>Student Discount</li>
+                    <li>Refer a Friend</li>
+                </ul>
+
+                <ul className="list_3">
+                    <li><h3>About</h3></li>
+                    <li>Company</li>
+                    <li>Corporate News</li>
+                    <li>Press Center</li>
+                    <li>#REFORM</li>
+                    <li>Investors</li>
+                    
+                </ul>
+
+                <ul className="list_4">
+                    <li><h3>STAY UP TO DATE</h3></li>
+                    <li>Sign Up For Email</li>
+                   
+                </ul>
+
+
+            
+   
+            </div>
+        </footer>
+    )
+}
